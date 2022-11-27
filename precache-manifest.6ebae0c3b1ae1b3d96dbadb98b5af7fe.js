d4c0ecd1d6d4087b9d25deeddd732d1088990bb7
Elanor F <6finlayson@gmail.com>
2022-11-27 11:55:52 -0500

    Updates

---
+++ precache-manifest.6ebae0c3b1ae1b3d96dbadb98b5af7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e41ba24565053c08094a",
+    "url": "/crackergame.github.io/static/css/main.6cea8305.chunk.css"
+  },
+  {
+    "revision": "e41ba24565053c08094a",
+    "url": "/crackergame.github.io/static/js/main.e41ba245.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/crackergame.github.io/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "4a2a22489d72857e3768",
+    "url": "/crackergame.github.io/static/js/2.4a2a2248.chunk.js"
+  },
+  {
+    "revision": "05960aa8a8af5c0ef541",
+    "url": "/crackergame.github.io/static/js/runtime~main.05960aa8.js"
+  },
+  {
+    "revision": "89b840f02c5b9e8a8842ff7bf0ffa72b",
+    "url": "/crackergame.github.io/index.html"
+  }
+];
